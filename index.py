975f1d9233693b93992fc1b4e246a2852c4fac9a
BrunoMauricio <brunomauricio98@hotmail.com>
2019-02-28 19:11:34 +0000

    q

--- index.py
+++ index.py
@@ -10,17 +10,17 @@ else:
 
 FileLines = fd.read().split('\n')
 print(FileLines)
-Dic = OrderedDict()
+AllTags = OrderedDict()
 
-def AddToDict(Tag):
-    if Tag in list(Dic.keys()):
-        return Dic[Tag]
+def AddToTag(Tag):
+    if Tag in list(AllTags.keys()):
+        return AllTags[Tag]
     else:
-        if len(list(Dic.keys())):
-            Dic[Tag] = Dic[list(Dic.keys())[-1]]+1
-            return Dic[Tag]
+        if len(list(AllTags.keys())):
+            AllTags[Tag] = AllTags[list(AllTags.keys())[-1]]+1
+            return AllTags[Tag]
         else:
-            Dic[Tag] = 0
+            AllTags[Tag] = 0
             return 0
 
 FotoAmmount = int(FileLines[0])
@@ -37,8 +37,16 @@ for Photo in AllPhotos:
     #print([Slide[x+1] for x in range(int(Slide[1])-1)])
     templ = list()
     # H == 0  V == 1
+    templ.append(0)
     templ.append(1 if Photo[0] == "V" else 0)
     templ.append(int(Photo[1]))
-    ParsedPhotos.append(templ+[AddToDict(Photo[x+2]) for x in range(int(Photo[1]))])
-#print(Dic)
+    ParsedPhotos.append(templ+[AddToTag(Photo[x+2]) for x in range(int(Photo[1]))])
+
 print(ParsedPhotos)
+print(AllTags)
+
+for Tag in AllTags:
+    #templ = list()
+
+    #templ.append()
+    pass
