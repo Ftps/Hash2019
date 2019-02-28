24f474030bd5a88b1bd396811081529331c985f4
dbviana <up201606636@fe.up.pt>
2019-02-28 18:36:21 +0000

    output script done

--- index.py
+++ index.py
@@ -11,3 +11,16 @@ if len(sys.argv) == 2:
 	AllSlides = list(map(lambda x: x.split(" "), FileLines[1:(len(FileLines)-1)]))
 
 	print(AllSlides)
+
+slides = [1, 2, [3,4], 5, [6,7]]
+
+f = open("sadsd", "w")
+
+f.write('{}\n'.format(len(slides)))
+
+for i in slides:
+	try:
+		f.write('{} {}\n'.format(i[0], i[1]))
+	except:
+		#Int
+		f.write('{}\n'.format(i))
