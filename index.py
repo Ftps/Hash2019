329e1e346fef2ffbf3a90470d9c275651de912af
BrunoMauricio <brunomauricio98@hotmail.com>
2019-02-28 19:26:30 +0000

    q

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ else:
     fd  = open("Input", "r")
 
 FileLines = fd.read().split('\n')
-print(FileLines)
+#print(FileLines)
 AllTags = OrderedDict()
 
 def AddToTag(Tag):
@@ -31,22 +31,32 @@ AllPhotos = list(map(lambda x: x.split(" "), FileLines[1:(len(FileLines)-1)]))
 
 ParsedPhotos = list()
 
-ParsedTags = list()
-
 for Photo in AllPhotos:
     #print([Slide[x+1] for x in range(int(Slide[1])-1)])
     templ = list()
     # H == 0  V == 1
     templ.append(0)
     templ.append(1 if Photo[0] == "V" else 0)
-    templ.append(int(Photo[1]))
+    #templ.append(int(Photo[1]))
     ParsedPhotos.append(templ+[AddToTag(Photo[x+2]) for x in range(int(Photo[1]))])
 
-print(ParsedPhotos)
-print(AllTags)
+print(AllPhotos)
+print(AllTags)#Dictionary
+
+
+print(list(range(len(AllTags))))
+ParsedTags = [list() for x in range(len(AllTags))]
 
 for Tag in AllTags:
-    #templ = list()
+    #Search all tags
+    x = 0
+    for Photo in AllPhotos:
+        #Search all photos
+        if Tag in Photo[2:]:
+            #Tag is inside photo
+            #AllTags[Tag] is the tag id, in which we want to add
+            ParsedTags[AllTags[Tag]].append(x)
+        x+=1
 
-    #templ.append()
-    pass
+print(ParsedPhotos)
+print(ParsedTags)
