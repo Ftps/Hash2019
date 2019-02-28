7ed82958f2e247aaef7c7233f5826beda659492a
dbviana <up201606636@fe.up.pt>
2019-02-28 18:09:52 +0000

    index now accepts argument for file

--- index.py
+++ index.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
+import sys
 
-fd  = open("Input", "r")
-FileLines = fd.read().split('\n')
-print(FileLines)
+if len(sys.argv) == 2:
+	fd = open(sys.argv[1], "r")
+	FileLines = fd.read().split('\n')
+	print(FileLines)
 
-FotoAmmount = int(FileLines[0])
+	FotoAmmount = int(FileLines[0])
 
-AllSlides = list(map(lambda x: x.split(" "), FileLines[1:(len(FileLines)-1)]))
+	AllSlides = list(map(lambda x: x.split(" "), FileLines[1:(len(FileLines)-1)]))
 
-print(AllSlides)
+	print(AllSlides)
