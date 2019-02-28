6a3c05dd304d7063c896f4db038e7a510166b9d9
BrunoMauricio <brunomauricio98@hotmail.com>
2019-02-28 18:49:38 +0000

    q

--- index.py
+++ index.py
@@ -13,25 +13,36 @@ print(FileLines)
 Dic = OrderedDict()
 
 def AddToDict(Tag):
-    if Tag in Dic.keys():
+    print("Tag: "+str(Tag))
+    if Tag in list(Dic.keys()):
+        print("Tag already exists, ID: "+str(Dic[Tag]))
         return Dic[Tag]
     else:
-        if len(Dic.keys()):
-            Dic[Tag] = Dic[Dicd.keys()[-1]]+1
+        if len(list(Dic.keys())):
+            print("Is not empty, adding as "+str(Dic[list(Dic.keys())[-1]]+1))
+            Dic[Tag] = Dic[list(Dic.keys())[-1]]+1
+            return Dic[Tag]
         else:
+            print("IS empty, adding as 0")
             Dic[Tag] = 0
+            return 0
 
 FotoAmmount = int(FileLines[0])
 
 AllSlides = list(map(lambda x: x.split(" "), FileLines[1:(len(FileLines)-1)]))
 
-print(AllSlides)
+#print(AllSlides)
 
 ParsedSlides = list()
+
+ParsedTags = list()
+
 for Slide in AllSlides:
     #print([Slide[x+1] for x in range(int(Slide[1])-1)])
-    templ = list(Slide[0])
-    templ.append(Slide[1])
-    ParsedSlides.append(templ+[(Slide[x+2]) for x in range(int(Slide[1]))])
-
+    templ = list()
+    # H == 0  V == 1
+    templ.append(1 if Slide[0] == "V" else 0)
+    templ.append(int(Slide[1]))
+    ParsedSlides.append(templ+[AddToDict(Slide[x+2]) for x in range(int(Slide[1]))])
+#print(Dic)
 print(ParsedSlides)
