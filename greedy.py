f6292108730e93d8fa7df661a1f2e695626456c6
Ftps <francisco.tomas@tecnico.ulisboa.pt>
2019-02-28 19:43:21 +0000

    correct

--- greedy.py
+++ greedy.py
@@ -4,7 +4,7 @@ from index.py import *
 from output.py import *
 from math import modf
 
-MAX = 100000
+MAX = 1000000.0
 
 def score(photo1, photo2):
     s0 = 0
@@ -13,11 +13,11 @@ def score(photo1, photo2):
     for x in range(len(photo1)-2):
         for y in range(len(photo2)-2):
             if photo1[x+2] == photo2[y+2]:
-                s0++
-                s1--
-                s2--
+                s0+=1
+                s1-=1
+                s2-=1
 
-    return min(s0, s1, s2);
+    return min(s0, s1, s2)
 
 cur = 0
 ParsedPhotos[0][0] = 1
@@ -25,7 +25,8 @@ slides = [0]
 
 for i in range(len(ParsedPhotos)-1):
     scores = []
-    if ParsedPhotos[cur][1]:
+    if ParsedPhotos[cur][1] == 1:
+        oldcur = cur
         pass
     else:
         for x in range(len(ParsedPhotos[cur])-2):
@@ -35,5 +36,9 @@ for i in range(len(ParsedPhotos)-1):
                     continue
                 scores.append(score(ParsedPhotos[y], ParsedPhotos[cur])+y/MAX)
 
-        slides.append(modf(max(scores))[0]*MAX)
-        ParsedPhotos[slides[i+1]][0] = 1
+        cur = modf(max(scores))[0]*MAX
+        if ParsedPhotos[cur][1] == 0:
+            slides.append(cur)
+        else:
+            oldcur = 1
+        ParsedPhotos[cur][0] = 1
